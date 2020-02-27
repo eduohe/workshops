543d60be2440a38dd9633e61f9502081cf761bbd
Eduardo Ohe <eohe@junglescout.com>
2020-02-27 15:05:36 -0800

    dynamo

--- dynamodb/crud/routes/products.js
+++ dynamodb/crud/routes/products.js
@@ -9,7 +9,7 @@ var AWS = require("aws-sdk");
 // });
 
 var credentials = new AWS.SharedIniFileCredentials({
-  profile: 'sandbox-workshop'
+  profile: 'default'
 });
 AWS.config.update({
   region: "us-west-2"
@@ -43,5 +43,178 @@ router.get('/read', function (req, res, next) {
 
 });
 
+
+router.get('/create', function (req, res, next) {
+  var docClient = new AWS.DynamoDB.DocumentClient();
+
+  var params = {
+    TableName: TABLE_NAME,
+    Item: {
+      "id": 2,
+      "category": 'Office',
+      "name": 'Pencil',
+      "price": 2.34
+    }
+  };
+
+  docClient.put(params, function (err, data) {
+    if (err) {
+      res.send('Unable to add record' + '. Error :' + JSON.stringify(err, null, 2));
+    } else {
+      res.send('PutItem succeeded');
+    }
+  });
+
+});
+
+router.get('/delete', function (req, res, next) {
+  var docClient = new AWS.DynamoDB.DocumentClient();
+
+  var params = {
+    TableName: TABLE_NAME,
+    Key: {
+      "id": 1
+    }
+  };
+
+  docClient.delete(params, function (err, data) {
+    if (err) {
+      res.send('Unable to delete item. Error:' + JSON.stringify(err, null, 2));
+    } else {
+      res.send('DeleteItem succeeded');
+    }
+  });
+
+});
+
+router.get('/update', function (req, res, next) {
+  var docClient = new AWS.DynamoDB.DocumentClient();
+
+  var params = {
+    TableName: TABLE_NAME,
+    Item: {
+      "id": 2,
+      "category": 'Office',
+      "name": 'Pencil',
+      "price": 3.45
+    }
+  };
+
+  // Same method as Create
+  docClient.put(params, function (err, data) {
+    if (err) {
+      res.send('Unable to update record' + '. Error :' + JSON.stringify(err, null, 2));
+    } else {
+      res.send('PutItem succeeded');
+    }
+  });
+
+});
+
+
+router.get('/scan', function (req, res, next) {
+  var docClient = new AWS.DynamoDB.DocumentClient();
+
+  var params = {
+    TableName: TABLE_NAME
+  };
+
+  docClient.scan(params, function (err, data) {
+    if (err) {
+      res.send("Unable to query. Error:" + JSON.stringify(err, null, 2));
+    } else {
+      console.log("Query succeeded.");
+      data.Items.forEach(function (item) {
+        res.send(item.id + ": " + item.name + ' ' + item.category);
+      });
+    }
+  });
+
+});
+
+
+router.get('/query', function (req, res, next) {
+  var docClient = new AWS.DynamoDB.DocumentClient();
+
+  var params = {
+    TableName: TABLE_NAME,
+    IndexName: "category-index",
+    ExpressionAttributeValues: {
+      ":category": "Office"
+    },
+    KeyConditionExpression: "category = :category"
+  };
+
+  docClient.query(params, function (err, data) {
+    if (err) {
+      res.send("Unable to query. Error:" + JSON.stringify(err, null, 2));
+    } else {
+      console.log("Query succeeded.");
+      res.send(data);
+    }
+  });
+
+});
+
+
+router.get('/mass_write', function (req, res, next) {
+  var docClient = new AWS.DynamoDB.DocumentClient();
+
+  for (i = 0; i <= 100000; i++) {
+    var params = {
+      TableName: TABLE_NAME,
+      Item: {
+        "id": i,
+        "category": 'Office',
+        "name": 'Pencil',
+        "price": i
+      }
+    };
+
+    docClient.put(params, function (err, data) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  }
+
+  res.send("Finished.");
+});
+
+
+router.get('/transaction', function (req, res, next) {
+  var docClient = new AWS.DynamoDB.DocumentClient();
+
+  const params = {
+    TransactItems: [{
+      Put: {
+        TableName: TABLE_NAME,
+        Item: {
+          "id": 1,
+          "category": 'Office',
+          "name": 'Pencil',
+          "price": 3.45
+        }
+      }
+    }, {
+      Put: {
+        TableName: TABLE_NAME,
+        Item: {
+          "id": 2,
+          "category": 'Office',
+          "name": 'Pencil',
+          "price": 4.56
+        }
+      }
+    }]
+  };
+
+  docClient.transactWrite(params, function (err, data) {
+    if (err) console.log(err);
+    else console.log(data);
+  });
+
+  res.send("Finished.");
+});
 
 module.exports = router;
